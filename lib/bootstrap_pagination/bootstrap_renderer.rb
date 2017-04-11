e3446ba65330343335466bc3baba633c839b9eaf
cd-rum <txnash@gmail.com>
2017-04-11 14:40:06 -0400

    specs fixed

--- lib/bootstrap_pagination/bootstrap_renderer.rb
+++ lib/bootstrap_pagination/bootstrap_renderer.rb
@@ -48,12 +48,12 @@ module BootstrapPagination
 
     def previous_page
       num = @collection.current_page > 1 && @collection.current_page - 1
-      previous_or_next_page(num, @options[:previous_label], 'page-item')
+      previous_or_next_page(num, @options[:previous_label], 'prev page-item')
     end
 
     def next_page
       num = @collection.current_page < @collection.total_pages && @collection.current_page + 1
-      previous_or_next_page(num, @options[:next_label], 'page-item')
+      previous_or_next_page(num, @options[:next_label], 'next page-item')
     end
 
     def ul_class
